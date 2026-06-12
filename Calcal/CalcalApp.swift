ec3515203f265f19692cf90ceae427145fecec62
Dmitryi <dm.niki.q@gmail.com>
2026-06-12 10:57:47 +0300

    Initial commit from Cursor

---
+++ Calcal/CalcalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcalApp.swift
+//  Calcal
+//
+//  Created by Dmitriy on 12.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct CalcalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
